482ff7c93ca2a5110bd55f82af8da10ab41ccb7b
Emmanuel Granara <emanuelgranara@hotmail.com>
2021-09-29 13:14:17 -0300

    Pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Pagination = ({ tasksPerPage, totalTasks, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalTasks / tasksPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav className="m-auto mt-2 text-center" style={{cursor: "pointer"}}>
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} className="page-item">
+            <a onClick={() => paginate(number)} className="page-link">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,29 @@
-import React, { useEffect } from "react";
-import TodoItem from "./TodoItem";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllTasks } from "../redux/todoSlice";
+
+import TodoItem from "./TodoItem";
+import Pagination from './Pagination'
 
 const TodoList = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [tasksPerPage] = useState(20);
+
+  const indexOfLastTask = currentPage * tasksPerPage;
+  const indexOfFirstTask = indexOfLastTask - tasksPerPage;
+  const currentTasks = todos.slice(indexOfFirstTask, indexOfLastTask);
 
   useEffect(() => {
     dispatch(getAllTasks());
   }, [dispatch]);
 
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <ul className="list-group mb-2">
-      {todos.map((todo) => (
+      {currentTasks.map((todo) => (
         <TodoItem
           key={todo.id}
           userId={todo.userId}
@@ -21,7 +31,8 @@ const TodoList = () => {
           title={todo.title}
           completed={todo.completed}
         />
-      ))}
+        ))}
+        <Pagination tasksPerPage={tasksPerPage} totalTasks={todos.length} paginate={paginate} />
     </ul>
   );
 };
